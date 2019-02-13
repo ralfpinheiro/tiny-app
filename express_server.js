2d17803f1e5858974c48d9034c253b9419088df0
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-13 00:50:39 +0000

    Included body parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,10 +27,21 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     res.render("urls_show", templateVars);
   });
+
+  
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
